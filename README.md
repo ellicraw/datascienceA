fb7643553b9743d836d9ee90ee6988e03f772cb8
kezzia <mvandertramps@gmail.com>
2018-03-28 07:22:28 -0400

    basic scatter plot function and histogram function
    Scatter plot function does not yet have a trend line

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # datascienceA
-Group A 
+Group A
+
+To run this project:
+- This project uses Python 3.6 and Anaconda (for pandas, IPython, numpy,
+  matplotlib etc). Download Anaconda here: https://www.anaconda.com/download/
+- Open "Anaconda Prompt" from the start menu and enter the command
+  "python team_a.py"
--- team_a.py
+++ team_a.py
@@ -1,6 +1,34 @@
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
+def make_scatter_plot(dataframe, x_axis, y_axis):
+  #Creates a scatter plot of x_axis vs y_axis along with model names.
+
+  # Generate the scatter plot
+  x = dataframe[x_axis]
+  y = dataframe[y_axis]
+  plt.ylabel(y_axis)
+  plt.xlabel(x_axis)
+  plt.scatter(x, y, color='black', label="")
+
+def make_histogram(param):
+  plt.figure(figsize=(20, 5))
+  plt.subplot(1, 3, 1)
+  plt.title(param)
+  histogram = antelope_dataframe[param].hist(bins=50)
+
+def make_cumulative_trend_plot():
+  print("nothing yet")
 
 # Load in the data from a CSV file that is comma seperated.
-antelope_data = pd.read_csv('data_set.csv', sep=',', names=cols, header=None, encoding='latin-1')
+antelope_dataframe = pd.read_csv('data_set.csv', sep=',')
+antelope_dataframe.describe()
+print('Spring Fawn Count vs Winter Severity')
+make_scatter_plot(antelope_dataframe, 'X1', 'X4')
+make_histogram("X1")
+make_cumulative_trend_plot()
+plt.show()
