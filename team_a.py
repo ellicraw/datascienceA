f1103b32585f33ee6539294d97c2e1be13962099
kezzia <mvandertramps@gmail.com>
2018-03-28 08:06:37 -0400

    Shows all the histograms together

--- team_a.py
+++ team_a.py
@@ -15,11 +15,23 @@ def make_scatter_plot(dataframe, x_axis, y_axis):
   plt.xlabel(x_axis)
   plt.scatter(x, y, color='black', label="")
 
-def make_histogram(param):
+def make_histogram(param1, param2, param3, param4):
   plt.figure(figsize=(20, 5))
-  plt.subplot(1, 3, 1)
-  plt.title(param)
-  histogram = antelope_dataframe[param].hist(bins=50)
+  plt.subplot(1, 4, 1)
+  plt.title(param1)
+  histogram = antelope_dataframe[param1].hist(bins=50)
+
+  plt.subplot(1, 4, 2)
+  plt.title(param2)
+  histogram = antelope_dataframe[param2].hist(bins=50)
+
+  plt.subplot(1, 4, 3)
+  plt.title(param3)
+  histogram = antelope_dataframe[param3].hist(bins=50)
+
+  plt.subplot(1, 4, 4)
+  plt.title(param4)
+  histogram = antelope_dataframe[param4].hist(bins=50)
 
 def make_cumulative_trend_plot():
   print("nothing yet")
@@ -29,6 +41,7 @@ antelope_dataframe = pd.read_csv('data_set.csv', sep=',')
 antelope_dataframe.describe()
 print('Spring Fawn Count vs Winter Severity')
 make_scatter_plot(antelope_dataframe, 'X1', 'X4')
-make_histogram("X1")
-make_cumulative_trend_plot()
+make_histogram("X1", "X2", "X3", "X4")
+#make_cumulative_trend_plot()
+
 plt.show()
